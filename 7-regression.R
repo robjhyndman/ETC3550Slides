b452468647de866e9f69658361f65fc821738d8b
Rob Hyndman <robjhyndman@gmail.com>
2020-05-28 08:47:50 +1000

    Updated ch7 code

--- 7-regression.R
+++ 7-regression.R
@@ -29,7 +29,7 @@ us_change %>%
 
 us_change %>%
   as_tibble() %>%
-  select(-Quarter) %>%
+  select(Income, Production, Savings, Unemployment, Consumption) %>%
   GGally::ggpairs()
 
 fit_consMR <- us_change %>%
@@ -52,8 +52,7 @@ augment(fit_consMR) %>%
     ggtitle("Percentage change in US consumption expenditure") +
     geom_abline(intercept=0, slope=1)
 
-augment(fit_consMR) %>%
-  gg_tsdisplay(.resid, plot_type="hist")
+gg_tsresiduals(fit_consMR)
 
 fit_consBest <- us_change %>%
   model(
@@ -65,24 +64,26 @@ down_future <- new_data(us_change, 4) %>%
 fc_down <- forecast(fit_consBest, new_data = down_future)
 
 up_future <- new_data(us_change, 4) %>%
-  mutate(Income = 1, Savings = 0.5, Unemployment = 0)
+  mutate(Income = c(1,2,3,3), Savings = 0.5, Unemployment = 0)
 fc_up <- forecast(fit_consBest, new_data = up_future)
 
 us_change %>% autoplot(Consumption) +
   ylab("% change in US consumption") +
-  autolayer(fc_up, series = "increase") +
-  autolayer(fc_down, series = "decrease") +
+  autolayer(fc_up) +
+  autolayer(fc_down) +
   guides(colour = guide_legend(title = "Scenario"))
 
 
 ## BEER ---------------------------------------------------------------------
 
-recent_production <- aus_production %>% filter(year(Quarter) >= 1992)
+recent_production <- aus_production %>%
+  filter(year(Quarter) >= 1992)
 recent_production %>% autoplot(Beer) +
   xlab("Year") + ylab("Megalitres") +
   ggtitle("Australian quarterly beer production")
 
-fit_beer <- recent_production %>% model(TSLM(Beer ~ trend() + season()))
+fit_beer <- recent_production %>%
+  model(TSLM(Beer ~ trend() + season()))
 report(fit_beer)
 
 augment(fit_beer) %>%
@@ -100,15 +101,23 @@ augment(fit_beer) %>%
     scale_colour_brewer(palette="Dark2", name="Quarter") +
     geom_abline(intercept=0, slope=1)
 
-augment(fit_beer) %>% gg_tsdisplay(.resid, plot_type="hist")
+gg_tsresiduals(fit_beer)
 
-fit_beer %>% forecast %>% autoplot(recent_production)
+fit_beer %>%
+  forecast(h="3 years") %>%
+  autoplot(recent_production)
 
-fourier_beer <- recent_production %>% model(TSLM(Beer ~ trend() + fourier(K=2)))
+fourier_beer <- recent_production %>%
+  model(TSLM(Beer ~ trend() + fourier(K=2)))
+glance(fourier_beer)
 report(fourier_beer)
 
 recent_production <- aus_production %>% filter(year(Quarter) >= 1992)
-fit_beer <- recent_production %>% model(TSLM(Beer ~ trend() + season()))
+fit_beer <- recent_production %>%
+  model(
+    season = TSLM(Beer ~ trend() + season()),
+    fourier = TSLM(Beer ~ trend() + fourier(K=2))
+  )
 fc_beer <- forecast(fit_beer)
 fc_beer %>% autoplot(recent_production) +
   ggtitle("Forecasts of beer production using regression") +
@@ -124,6 +133,7 @@ marathon <- boston_marathon %>%
 marathon %>% autoplot(Minutes) +
   xlab("Year") +  ylab("Winning times in minutes")
 
+
 fit_trends <- marathon %>%
   model(
     # Linear trend
@@ -131,21 +141,25 @@ fit_trends <- marathon %>%
     # Exponential trend
     exponential = TSLM(log(Minutes) ~ trend()),
     # Piecewise linear trend
-    piecewise = TSLM(Minutes ~ trend(knots = c(1940, 1980)))
+    piecewise = TSLM(Minutes ~ trend(knots = c(1940, 1980))),
+    better = TSLM(Minutes ~ trend(knots = c(1919,1930,1940,1980)))
   )
+glance(fit_trends) %>%
+  select(.model, r_squared, adj_r_squared, AICc, BIC, CV, log_lik)
+
 
 fit_trends %>%
   forecast(h=10) %>%
-  autoplot(marathon, alpha = 0.3) +
-  geom_line(aes(y = .fitted, colour = .model), data = fitted(fit_trends)) +
+  autoplot(marathon, alpha = 0.5) +
+  geom_line(aes(y = .fitted, colour = .model), data = augment(fit_trends)) +
   labs(x = "Year", y = "Winning times in minutes", title = "Boston Marathon") +
   guides(colour=guide_legend(title=NULL))
 
 fit_trends %>%
   select(piecewise) %>%
-  augment() %>%
-  gg_tsdisplay(.resid, plot_type = "histogram")
+  gg_tsresiduals()
 
-glance(fit_trends)
+glance(fit_trends) %>%
+  select(.model, r_squared, adj_r_squared, AICc, BIC, CV, log_lik)
 
 
