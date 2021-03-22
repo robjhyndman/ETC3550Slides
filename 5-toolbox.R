2cbb71e6c0e68c7ecf362738bd3ec1ea89f03861
Rob Hyndman <robjhyndman@gmail.com>
2021-03-22 16:15:51 +1100

    Updated slides for ch5

--- 5-toolbox.R
+++ 5-toolbox.R
@@ -2,22 +2,29 @@ library(fpp3)
 
 ## ---- GDP ----------------------------------------------------------------
 
-global_economy %>%
-  filter(Country=="Sweden") %>%
-  autoplot(GDP) +
-    ggtitle("GDP for Sweden") + ylab("$US billions")
+gdppc <- global_economy %>%
+  mutate(GDP_per_capita = GDP / Population) %>%
+  select(Year, Country, GDP, Population, GDP_per_capita)
+gdppc
 
-fit <- global_economy %>%
+gdppc %>%
+  filter(Country == "Sweden") %>%
+  autoplot(GDP_per_capita) +
+  labs(title = "GDP per capita for Sweden", y = "$US")
+
+fit <- gdppc %>%
   model(trend_model = TSLM(GDP ~ trend()))
 
-fit %>% forecast(h = "3 years") %>%
-  filter(Country=="Sweden") %>%
+fc <- fit %>% forecast(h = "3 years")
+
+fc %>%
+  filter(Country == "Sweden") %>%
   autoplot(global_economy) +
-    ggtitle("GDP for Sweden") + ylab("$US billions")
+  labs(title = "GDP per capita for Sweden", y = "$US")
 
 ## ---- Bricks ------------------------------------------------------------
 
-brick_fit <-  aus_production %>%
+brick_fit <- aus_production %>%
   filter(!is.na(Bricks)) %>%
   model(
     Seasonal_naive = SNAIVE(Bricks),
@@ -31,8 +38,10 @@ brick_fc <- brick_fit %>%
 
 brick_fc %>%
   autoplot(aus_production, level = NULL) +
-  ggtitle("Forecasts for quarterly clay brick production") +
-  xlab("Year") + ylab("Millions of bricks") +
+  labs(
+    title = "Clay brick production for Australia",
+    y = "Millions of bricks"
+  ) +
   guides(colour = guide_legend(title = "Forecast"))
 
 
@@ -42,9 +51,13 @@ brick_fc %>%
 fb_stock <- gafa_stock %>%
   filter(Symbol == "FB") %>%
   mutate(trading_day = row_number()) %>%
-  update_tsibble(index=trading_day, regular=TRUE)
+  update_tsibble(index = trading_day, regular = TRUE)
 
-fb_stock %>% autoplot(Close)
+fb_stock %>% autoplot(Close) +
+  labs(
+    title = "Facebook closing stock price",
+    y = "$US"
+  )
 
 # Specify, estimate and forecast
 fb_stock %>%
@@ -53,40 +66,43 @@ fb_stock %>%
     Naive = NAIVE(Close),
     Drift = RW(Close ~ drift())
   ) %>%
-  forecast(h=42) %>%
+  forecast(h = 42) %>%
   autoplot(fb_stock, level = NULL) +
-  ggtitle("Facebook closing stock price") +
-  xlab("Day") + ylab("") +
-  guides(colour=guide_legend(title="Forecast"))
+  labs(
+    title = "Facebook closing stock price",
+    y = "$US"
+  ) +
+  guides(colour = guide_legend(title = "Forecast"))
 
 fit <- fb_stock %>% model(NAIVE(Close))
 
 augment(fit) %>%
-  filter(trading_day > 1200) %>%
+  # filter(trading_day > 1100) %>%
   ggplot(aes(x = trading_day)) +
   geom_line(aes(y = Close, colour = "Data")) +
   geom_line(aes(y = .fitted, colour = "Fitted"))
 
 augment(fit) %>%
-  autoplot(.resid) + xlab("Day") + ylab("") +
-  ggtitle("Residuals from naïve method")
+  autoplot(.resid) +
+  labs(
+    y = "$US",
+    title = "Residuals from naïve method"
+  )
 
 augment(fit) %>%
   ggplot(aes(x = .resid)) +
   geom_histogram(bins = 150) +
-  ggtitle("Histogram of residuals")
+  labs(title = "Histogram of residuals")
 
 augment(fit) %>%
   ACF(.resid) %>%
-  autoplot() + ggtitle("ACF of residuals")
+  autoplot() +
+  labs(title = "ACF of residuals")
 
 gg_tsresiduals(fit)
 
-
-## -- Finished to here on 9 April
-
 augment(fit) %>%
-  features(.resid, ljung_box, lag=10, dof=0)
+  features(.resid, ljung_box, lag = 10, dof = 0)
 
 
 ## BEER -------------------------------------------
@@ -94,14 +110,18 @@ augment(fit) %>%
 recent <- aus_production %>% filter(year(Quarter) >= 1992)
 recent %>% autoplot(Beer)
 fit <- recent %>% model(SNAIVE(Beer))
-fit %>% forecast() %>% autoplot(recent)
+fit %>%
+  forecast() %>%
+  autoplot(recent)
 
 augment(fit) %>%
-  features(.resid, ljung_box, lag=10, dof=0)
+  features(.resid, ljung_box, lag = 10, dof = 0)
 
 gg_tsresiduals(fit)
 
-fit %>% forecast %>% hilo(level = c(50,95))
+fit %>%
+  forecast() %>%
+  hilo(level = c(50, 95))
 
 ## FOOD RETAILING
 
@@ -122,15 +142,35 @@ fc %>% autoplot(filter(food, year(Month) > 2010))
 
 ## EGG PRICES
 
-eggs <- as_tsibble(fma::eggs)
-autoplot(eggs)
-fit <- eggs %>% model(RW(log(value) ~ drift()))
-fc <- fit %>% forecast(h=50)
-fc_biased <- fit %>% forecast(h=50, bias_adjust = FALSE)
-eggs %>% autoplot(value) + xlab("Year") +
-  autolayer(fc_biased, level = 80) +
-  autolayer(fc, colour = "red", level = NULL)
+eggs <- prices %>%
+  filter(!is.na(eggs)) %>%
+  select(eggs)
+eggs %>%
+  autoplot(eggs) +
+  labs(
+    title = "Annual egg prices",
+    y = "$US (adjusted for inflation)"
+  )
 
+fit <- eggs %>%
+  model(RW(log(eggs) ~ drift()))
+fit
+fc <- fit %>%
+  forecast(h = 50)
+fc
+
+fc %>% autoplot(eggs) +
+  labs(
+    title = "Annual egg prices",
+    y = "US$ (adjusted for inflation)"
+  )
+
+fc %>%
+  autoplot(eggs, level = 80, point_forecast = lst(mean, median)) +
+  labs(
+    title = "Annual egg prices",
+    y = "US$ (adjusted for inflation)"
+  )
 
 ## US RETAIL EMPLOYMENT
 
@@ -138,7 +178,8 @@ us_retail_employment <- us_employment %>%
   filter(year(Month) >= 1990, Title == "Retail Trade") %>%
   select(-Series_ID)
 
-autoplot(us_retail_employment, Employed)
+us_retail_employment %>%
+  autoplot(Employed)
 
 dcmp <- us_retail_employment %>%
   model(STL(Employed)) %>%
@@ -149,7 +190,7 @@ dcmp %>%
   model(NAIVE(season_adjust)) %>%
   forecast() %>%
   autoplot(dcmp) +
-  ggtitle("Naive forecasts of seasonally adjusted data")
+  labs(title = "Naive forecasts of seasonally adjusted data")
 
 us_retail_employment %>%
   model(stlf = decomposition_model(
@@ -170,12 +211,12 @@ train <- recent_production %>%
 beer_fit <- train %>%
   model(
     Mean = MEAN(Beer),
-    `Naïve` = NAIVE(Beer),
-    `Seasonal naïve` = SNAIVE(Beer),
+    Naive = NAIVE(Beer),
+    Seasonal_naive = SNAIVE(Beer),
     Drift = RW(Beer ~ drift())
   )
 beer_fc <- beer_fit %>%
-  forecast(h = 24)
+  forecast(h = 10)
 
 accuracy(beer_fc, recent_production)
 accuracy(beer_fit)
@@ -185,9 +226,10 @@ accuracy(beer_fit)
 fb_stock <- gafa_stock %>%
   filter(Symbol == "FB") %>%
   mutate(trading_day = row_number()) %>%
-  update_tsibble(index=trading_day, regular=TRUE)
+  update_tsibble(index = trading_day, regular = TRUE)
 
-fb_stock %>% autoplot(Close)
+fb_stock %>%
+  autoplot(Close)
 
 fb_stretch <- fb_stock %>%
   stretch_tsibble(.init = 3, .step = 1) %>%
@@ -197,11 +239,10 @@ fit_cv <- fb_stretch %>%
   model(RW(Close ~ drift()))
 
 fc_cv <- fit_cv %>%
-  forecast(h=1)
+  forecast(h = 1)
 
 fc_cv %>% accuracy(fb_stock)
 
 fb_stock %>%
   model(RW(Close ~ drift())) %>%
   accuracy()
-
