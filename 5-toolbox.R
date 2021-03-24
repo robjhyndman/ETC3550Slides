715f432d3ae7e61635c01d9792f8933d5c29be18
Rob J Hyndman <robjhyndman@gmail.com>
2021-03-24 14:01:19 +1100

    Updated R code used in lectures for ch5

--- 5-toolbox.R
+++ 5-toolbox.R
@@ -77,7 +77,7 @@ fb_stock %>%
 fit <- fb_stock %>% model(NAIVE(Close))
 
 augment(fit) %>%
-  # filter(trading_day > 1100) %>%
+  filter(trading_day > 1100) %>%
   ggplot(aes(x = trading_day)) +
   geom_line(aes(y = Close, colour = "Data")) +
   geom_line(aes(y = .fitted, colour = "Fitted"))
@@ -104,40 +104,13 @@ gg_tsresiduals(fit)
 augment(fit) %>%
   features(.resid, ljung_box, lag = 10, dof = 0)
 
-
-## BEER -------------------------------------------
-
-recent <- aus_production %>% filter(year(Quarter) >= 1992)
-recent %>% autoplot(Beer)
-fit <- recent %>% model(SNAIVE(Beer))
-fit %>%
-  forecast() %>%
-  autoplot(recent)
-
-augment(fit) %>%
-  features(.resid, ljung_box, lag = 10, dof = 0)
-
-gg_tsresiduals(fit)
-
-fit %>%
-  forecast() %>%
-  hilo(level = c(50, 95))
-
-## FOOD RETAILING
-
-food <- aus_retail %>%
-  filter(Industry == "Food retailing") %>%
-  summarise(Turnover = sum(Turnover))
-
-fit <- food %>%
-  model(SNAIVE(log(Turnover)))
-
-fc <- fit %>%
-  forecast(h = "3 years")
-
-fc %>% autoplot(food)
-
-fc %>% autoplot(filter(food, year(Month) > 2010))
+fc <- fb_stock %>%
+  model(
+    Mean = MEAN(Close),
+    Naive = NAIVE(Close),
+    Drift = RW(Close ~ drift())
+  ) %>%
+  forecast(h = 42)
 
 
 ## EGG PRICES
@@ -153,7 +126,7 @@ eggs %>%
   )
 
 fit <- eggs %>%
-  model(RW(log(eggs) ~ drift()))
+  model(rwdrift = RW(log(eggs) ~ drift()))
 fit
 fc <- fit %>%
   forecast(h = 50)
@@ -172,77 +145,3 @@ fc %>%
     y = "US$ (adjusted for inflation)"
   )
 
-## US RETAIL EMPLOYMENT
-
-us_retail_employment <- us_employment %>%
-  filter(year(Month) >= 1990, Title == "Retail Trade") %>%
-  select(-Series_ID)
-
-us_retail_employment %>%
-  autoplot(Employed)
-
-dcmp <- us_retail_employment %>%
-  model(STL(Employed)) %>%
-  components() %>%
-  select(-.model)
-
-dcmp %>%
-  model(NAIVE(season_adjust)) %>%
-  forecast() %>%
-  autoplot(dcmp) +
-  labs(title = "Naive forecasts of seasonally adjusted data")
-
-us_retail_employment %>%
-  model(stlf = decomposition_model(
-    STL(Employed ~ trend(window = 7), robust = TRUE),
-    NAIVE(season_adjust)
-  )) %>%
-  forecast() %>%
-  autoplot(us_retail_employment)
-
-
-## BEER PRODUCTION
-
-recent_production <- aus_production %>%
-  filter(year(Quarter) >= 1992)
-recent_production %>% autoplot(Beer)
-train <- recent_production %>%
-  filter(year(Quarter) <= 2007)
-beer_fit <- train %>%
-  model(
-    Mean = MEAN(Beer),
-    Naive = NAIVE(Beer),
-    Seasonal_naive = SNAIVE(Beer),
-    Drift = RW(Beer ~ drift())
-  )
-beer_fc <- beer_fit %>%
-  forecast(h = 10)
-
-accuracy(beer_fc, recent_production)
-accuracy(beer_fit)
-
-## CROSS-VALIDATION: FACEBOOK
-
-fb_stock <- gafa_stock %>%
-  filter(Symbol == "FB") %>%
-  mutate(trading_day = row_number()) %>%
-  update_tsibble(index = trading_day, regular = TRUE)
-
-fb_stock %>%
-  autoplot(Close)
-
-fb_stretch <- fb_stock %>%
-  stretch_tsibble(.init = 3, .step = 1) %>%
-  filter(.id != max(.id))
-
-fit_cv <- fb_stretch %>%
-  model(RW(Close ~ drift()))
-
-fc_cv <- fit_cv %>%
-  forecast(h = 1)
-
-fc_cv %>% accuracy(fb_stock)
-
-fb_stock %>%
-  model(RW(Close ~ drift())) %>%
-  accuracy()
