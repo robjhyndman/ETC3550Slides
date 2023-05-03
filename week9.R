741c609b799ed5f391952b2c75353e156a3e0adf
Rob Hyndman <robjhyndman@gmail.com>
2023-05-03 15:51:51 +1000

    Week 9 code

--- week9.R
+++ week9.R
@@ -1,21 +1,5 @@
 library(fpp3)
 
-# Fit models to GDP for all countries
-
-fit <- global_economy |>
-  model(arima = ARIMA(log(GDP)))
-
-fit |>
-  filter(Country == "Australia") |>
-  gg_tsresiduals()
-
-fc <- fit |>
-  forecast(h = 5)
-
-fc |>
-  filter(Country == "Australia") |>
-  autoplot(global_economy)
-
 ## US leisure employment
 
 leisure <- us_employment |>
@@ -77,84 +61,6 @@ forecast(fit, h = 36) |>
     y = "Number of people (millions)"
   )
 
-## h02 drugs ----------------------------------------------------------------------
-
-h02 <- PBS |>
-  filter(ATC2 == "H02") |>
-  summarise(Cost = sum(Cost))
-
-h02 |> autoplot(Cost)
-
-## Models using logs
-
-h02 |> autoplot(log(Cost))
-h02 |> gg_tsdisplay(difference(log(Cost), 12), lag_max = 36, plot_type = "partial")
-
-# My best guess
-fit <- h02 |>
-  model(arima3000210 = ARIMA(log(Cost) ~ pdq(3, 0, 0) + PDQ(2, 1, 0)))
-report(fit)
-gg_tsresiduals(fit, lag_max = 36)
-augment(fit) |>
-  features(.innov, ljung_box, lag = 36, dof = 6)
-
-# Letting R choose
-fit <- h02 |> model(auto = ARIMA(log(Cost), stepwise = FALSE))
-report(fit)
-gg_tsresiduals(fit, lag_max = 36)
-augment(fit) |>
-  features(.innov, ljung_box, lag = 36, dof = 6)
-
-# Letting R work hard to choose
-fit <- h02 |>
-  model(best = ARIMA(log(Cost),
-                     stepwise = FALSE,
-                     approximation = FALSE,
-                     order_constraint = p + q + P + Q <= 9 & (constant + d + D <= 2)
-  ))
-report(fit)
-gg_tsresiduals(fit, lag_max = 36)
-augment(fit) |>
-  features(.innov, ljung_box, lag = 36, dof = 9)
-
-# The forecasts
-fit |>
-  forecast() |>
-  autoplot(h02) +
-  labs(y = "H02 Expenditure ($AUD)")
-
-## AUS ECONOMY ETS vs ARIMA
-
-aus_economy <- global_economy |>
-  filter(Code == "AUS") |>
-  mutate(Population = Population / 1e6)
-aus_economy |> autoplot(Population)
-aus_economy |>
-  model(
-    ets = ETS(Population),
-    arima = ARIMA(Population)
-  ) |>
-  glance()
-aus_economy |>
-  slice(-n()) |>
-  stretch_tsibble(.init = 10) |>
-  model(
-    ets = ETS(Population),
-    arima = ARIMA(Population)
-  ) |>
-  forecast(h = 1) |>
-  accuracy(aus_economy) |>
-  select(.model, ME:RMSSE)
-
-aus_economy |>
-  model(ETS(Population)) |>
-  forecast(h = "5 years") |>
-  autoplot(aus_economy |> filter(Year > 2000)) +
-  labs(
-    title = "Australian population",
-    y = "People (millions)"
-  )
-
 # QUARTERLY CEMENT ETS vs ARIMA
 
 cement <- aus_production |>
@@ -187,15 +93,16 @@ gg_tsresiduals(fit |> select(ets), lag_max = 16)
 fit |>
   select(ets) |>
   augment() |>
-  features(.innov, ljung_box, lag = 16, dof = 6)
+  features(.innov, ljung_box, lag = 16)
 
 fit |>
   forecast(h = "2 years 6 months") |>
   accuracy(cement, level = 80)
 
-fit |>
-  select(arima) |>
-  forecast(h = "3 years") |>
+fc <- cement |>
+  model(arima = ARIMA(Cement)) |>
+  forecast(h = "3 years")
+fc |>
   autoplot(cement) +
   labs(
     title = "Cement production in Australia",
