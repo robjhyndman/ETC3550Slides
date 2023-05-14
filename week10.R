2a2e379e05b1682c865d6073d4f32ab10e52a632
Rob Hyndman <robjhyndman@gmail.com>
2023-05-15 09:38:29 +1000

    week 10 code

--- week10.R
+++ week10.R
@@ -1,5 +1,45 @@
 library(fpp3)
 
+# Weekly data with Fourier terms
+
+us_gasoline  |>
+  autoplot(Barrels) +
+  labs(y = "Weekly US finished motor gasoline product supplied (million barrels)")
+
+fit <- us_gasoline |>
+  model(
+    K01 = TSLM(log(Barrels) ~ trend(knots = yearweek(c("2006 W1", "2011 W1"))) + fourier(K = 1)),
+    K05 = TSLM(log(Barrels) ~ trend(knots = yearweek(c("2006 W1", "2011 W1"))) + fourier(K = 5)),
+    K06 = TSLM(log(Barrels) ~ trend(knots = yearweek(c("2006 W1", "2011 W1"))) + fourier(K = 6)),
+    K07 = TSLM(log(Barrels) ~ trend(knots = yearweek(c("2006 W1", "2011 W1"))) + fourier(K = 7)),
+    K08 = TSLM(log(Barrels) ~ trend(knots = yearweek(c("2006 W1", "2011 W1"))) + fourier(K = 8)),
+    K09 = TSLM(log(Barrels) ~ trend(knots = yearweek(c("2006 W1", "2011 W1"))) + fourier(K = 9)),
+    K10 = TSLM(log(Barrels) ~ trend(knots = yearweek(c("2006 W1", "2011 W1"))) + fourier(K = 10)),
+    K15 = TSLM(log(Barrels) ~ trend(knots = yearweek(c("2006 W1", "2011 W1"))) + fourier(K = 15)),
+    K20 = TSLM(log(Barrels) ~ trend(knots = yearweek(c("2006 W1", "2011 W1"))) + fourier(K = 20)),
+    K25 = TSLM(log(Barrels) ~ trend(knots = yearweek(c("2006 W1", "2011 W1"))) + fourier(K = 25))
+  )
+glance(fit) |>
+  select(.model, r_squared, adj_r_squared, df, AICc, CV) |>
+  arrange(CV)
+
+augment(fit) |>
+  filter(.model %in% c("K06", "K01", "K25")) |>
+  ggplot(aes(x = Week, y = Barrels)) +
+  geom_line() +
+  geom_line(aes(y = .fitted, col = .model)) +
+  facet_grid(.model ~ .)
+
+fit |>
+  select(K06) |>
+  forecast(h = "2 years") |>
+  autoplot(us_gasoline)
+
+fit |>
+  select(K06) |>
+  gg_tsresiduals()
+
+
 # US consumption quarterly changes
 
 us_change |>
@@ -92,36 +132,3 @@ us_change |> autoplot(Consumption) +
   labs(title = "US consumption", y = "% change")
 
 
-# Weekly data with Fourier terms
-
-us_gasoline  |>
-  autoplot(Barrels) +
-  labs(y = "Weekly US finished motor gasoline product supplied (million barrels)")
-
-fit <- us_gasoline |>
-  model(
-    K01 = TSLM(log(Barrels) ~ trend(knots = yearweek(c("2006 W1", "2011 W1"))) + fourier(K = 1)),
-    K05 = TSLM(log(Barrels) ~ trend(knots = yearweek(c("2006 W1", "2011 W1"))) + fourier(K = 5)),
-    K06 = TSLM(log(Barrels) ~ trend(knots = yearweek(c("2006 W1", "2011 W1"))) + fourier(K = 6)),
-    K07 = TSLM(log(Barrels) ~ trend(knots = yearweek(c("2006 W1", "2011 W1"))) + fourier(K = 7)),
-    K08 = TSLM(log(Barrels) ~ trend(knots = yearweek(c("2006 W1", "2011 W1"))) + fourier(K = 8)),
-    K09 = TSLM(log(Barrels) ~ trend(knots = yearweek(c("2006 W1", "2011 W1"))) + fourier(K = 9)),
-    K10 = TSLM(log(Barrels) ~ trend(knots = yearweek(c("2006 W1", "2011 W1"))) + fourier(K = 10)),
-    K15 = TSLM(log(Barrels) ~ trend(knots = yearweek(c("2006 W1", "2011 W1"))) + fourier(K = 15)),
-    K20 = TSLM(log(Barrels) ~ trend(knots = yearweek(c("2006 W1", "2011 W1"))) + fourier(K = 20)),
-    K25 = TSLM(log(Barrels) ~ trend(knots = yearweek(c("2006 W1", "2011 W1"))) + fourier(K = 25))
-  )
-glance(fit) |>
-  select(.model, r_squared, df, AICc, CV) |>
-  arrange(CV)
-
-augment(fit) |>
-  filter(.model %in% c("K06", "K01", "K25")) |>
-  ggplot(aes(x = Week, y = Barrels)) +
-  geom_line() +
-  geom_line(aes(y = .fitted, col = .model)) +
-  facet_grid(.model ~ .)
-
-fit |>
-  select(K06) |>
-  gg_tsresiduals()
